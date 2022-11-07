04e56833c7b2347c53a313333ec1f67354cdb20a
biulay <2273440122@qq.com>
2022-11-07 17:52:26 +0800

    Initial commit

---
+++ src/main/java/me/com/LoadConfig/Papi.java
@@ -0,0 +1,21 @@
+package me.com.LoadConfig;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+
+public class Papi extends PlaceholderExpansion {
+
+    @Override
+    public String getAuthor() {
+        return "Biulay";
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "Biulay";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+}
